440cd2960afa93b0718444f5fe2f4999d3daf716
Somayajula Chandra Sekhar <Somayajula.Sekhar@Triconinfotech.com>
2023-03-23 23:28:32 +0530

    feat: connected app to firebase

--- src/Request.js
+++ src/Request.js
@@ -1,6 +1,6 @@
 const API_KEY = "1be37d67b8344c700e5edd0ac37ea34c";
 const requests = {
-  fetchTrending: `/trending/all/weel?api_key=${API_KEY}&language=en-US`,
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
   fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
   fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
