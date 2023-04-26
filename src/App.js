7be369260ac8ce047130f73473d02f14d2d74034
Somayajula Chandra Sekhar <Somayajula.Sekhar@Triconinfotech.com>
2023-04-26 12:35:15 +0530

    removed LoginScreen component

--- src/App.js
+++ src/App.js
@@ -2,21 +2,25 @@ import React from "react";
 import "./App.css";
 import HomeScreen from "./HomeScreen";
 import Login from "./Login";
-import { BrowserRouter as Router, Route, Routes, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Switch,
+} from "react-router-dom";
 
 function App() {
-  const user =  null;
+  const user = { chandu: 1 };
   return (
     <div className="app">
       <Router>
-       <Routes>
-       {user ? (
-          <Route exact path="/" element={<HomeScreen />}>
-          </Route>
-        ) : (
-          <Route exact path="/" element={<Login />}></Route>
-        )}
-       </Routes>
+        <Routes>
+          {user ? (
+            <Route exact path="/" element={<HomeScreen />}></Route>
+          ) : (
+            <Route exact path="/" element={<Login />}></Route>
+          )}
+        </Routes>
       </Router>
       {/* <HomeScreen /> */}
     </div>
--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -16,12 +16,12 @@ function HomeScreen() {
         isLarge
       />
       <Rows title="Trending Now" fetchUrl={requests.fetchTrending} />
-      <Rows title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+      <Rows title="Top Rated" fetchUrl={requests.fetchTopRated} />
       <Rows title="Action Movies" fetchUrl={requests.fetchActionMovies} />
-      <Rows title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
-      <Rows title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
-      <Rows title="Romantic Movies"  fetchUrl={requests.fetchRomanceMovies}/>
-      <Rows title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
+      <Rows title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Rows title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Rows title="Romantic Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Rows title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
